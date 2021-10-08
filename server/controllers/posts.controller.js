dbb7fe16c2355943c45680bc331103e329f95065
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-10-08 17:08:42 +0300

    create method getpost and getposts in posts controller

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -4,11 +4,21 @@ import Post from "../models/Post.js";
 class PostsController {
 
     async getPosts(req, res) {
-
+        try {
+            const posts = await Post.find()
+            res.json(posts)
+        } catch (e) {
+            res.status(500).json('Server error: ', e.message)
+        }
     }
 
     async getPost(req, res) {
-
+        try {
+            const post = await Post.findById(req.params.id)
+            res.json(post)
+        } catch (e) {
+            res.status(500).json('Server error: ', e.message)
+        }
     }
 
     async addPost(req, res) {
