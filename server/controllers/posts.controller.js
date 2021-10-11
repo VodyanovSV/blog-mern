2353e6323ff53f1d2e7ec390b2e4adfce9318def
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-10-11 17:25:59 +0300

    create method addpost, deletepost and updateposts in posts controller

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -22,15 +22,31 @@ class PostsController {
     }
 
     async addPost(req, res) {
-
+        try {
+            const post = new Post({title: req.body.title, imageUrl: req.body.url, text: req.body.content})
+            await post.save()
+            res.json('Post has been created')
+        } catch (e) {
+            res.status(500).json('Server error: ', e.message)
+        }
     }
 
     async updatePost(req, res) {
-
+        try {
+            const data = await Post.updateOne({_id: req.params.id}, {title: req.body.title, imageUrl: req.body.imageUrl, text: req.body.text})
+            res.json(data)
+        } catch (e) {
+            res.status(500).json('Server error: ', e.message)
+        }
     }
 
     async deletePost(req, res) {
-
+        try {
+            const data = await Post.deleteOne({_id: req.params.id})
+            res.json(data)
+        } catch (e) {
+            res.status(500).json('Server error: ', e.message)
+        }
     }
 }
 
