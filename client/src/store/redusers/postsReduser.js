7026c4a38ad98851409fd6cbccc11e7b723c7c37
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-10-19 17:34:26 +0300

    create empty post reduser

---
+++ client/src/store/redusers/postsReduser.js
@@ -0,0 +1,11 @@
+
+
+const initialState = {
+    posts: []
+}
+
+export const postsReduser = (state = initialState, action) => {
+    switch (action.type) {
+
+    }
+}
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,10 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import {postsReduser} from "./redusers/postsReduser";
 
 const rootReduser = combineReducers({
-    
+    postsReduser
 })
 
 export const store = createStore(rootReduser, composeWithDevTools(applyMiddleware(thunk)))
