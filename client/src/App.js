e2820998c31a3de4a48fb3d0ef3e0e3a1c4afb3b
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-10-21 18:12:50 +0300

    create routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import {HeaderBlock} from './components'
+import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom'
 
 function App() {
+
+	const imageUrl = window.location.origin + '/images/bg_header.jpg'
 
     return (
         <div className={'App'}>
 			<HeaderBlock title={'Заголовок сайта'} description={'Описание'} imageUrl={imageUrl}/>
-
+			<div className="container">
+                <div className="content">
+                    <div className="content">
+                        <Router>
+                            <Routes>
+                                <Route path={'/'} exact element={}/>
+                            </Routes>
+                        </Router>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
