b9ba340d87cbff4d6caf9d19b5179e4b6a29817c
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-11-15 18:48:15 +0300

    add postitem to postlist

--- client/src/components/postList/PostsList.jsx
+++ client/src/components/postList/PostsList.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './PostsList.scss'
+import {PostItem} from "../index";
+import {useDispatch, useSelector} from "react-redux";
+import {setPosts} from "../../store/asyncActyons/asyncPosts";
 import {Link} from "react-router-dom";
 
 const PostsList = () => {
+
+    const dispatch = useDispatch()
+    const posts = useSelector(state => state.postsReduser.posts)
+
+    useEffect(() => {
+        dispatch(setPosts())
+    }, [])
 
     return (
         <div className="posts-list">
@@ -11,6 +21,14 @@ const PostsList = () => {
                     Add post
                 </button>
             </Link>
+            {posts.length ? posts.map((post) => (
+                    <div className={'post-items'} key={post._id}>
+                        <PostItem {...post}/>
+                    </div>
+                )) :
+                <div>
+                    Loading...
+                </div>}
         </div>
     );
 };
--- client/src/store/asyncActyons/asyncPosts.js
+++ client/src/store/asyncActyons/asyncPosts.js
@@ -1,3 +1,25 @@
+import {removePostActionCreators, setPostsActionCreators} from "../actionCreators/postsActionCreators";
+
+export const setPosts = () => {
+    return async (dispatch) => {
+        try {
+            const url = '/api/posts'
+            const method = 'GET'
+            const headers = {}
+
+            const response = await fetch(url, {method, headers})
+            const data = await response.json()
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Что-то пошло не так')
+            }
+
+            dispatch(setPostsActionCreators(data))
+        } catch (e) {
+            alert(e.message)
+        }
+    }
+}
 
 export const addPost = (post) => {
     return async (dispatch) => {
